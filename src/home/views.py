ba0b806c730745a2f24060a9865c02e76c56ed4a
Cesar <ces_mad@hotmail.es>
2014-03-17 14:39:11 -0500

    commit de prueba

--- src/home/views.py
+++ src/home/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, render_to_response
 from django.contrib.auth import login, logout, authenticate
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
-
+'''´ '''
 def index_view(request):
     return render_to_response('base.html', context_instance=RequestContext(request))
 
