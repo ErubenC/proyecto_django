cb83ba924581a1513f363966ecca61d07863e099
¨Ruben <rubenandres92@gmail.com>
2014-03-17 23:16:21 -0500

    agregando vista de  usuario basico (falta configurar opciones de grupos para administrar permisos de usuarios en la aplicacion)

--- src/home/forms.py
+++ src/home/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 '''
 class ContactForm(forms.Form):
     Email = forms.EmailField(widget=forms.TextInput())
@@ -9,3 +10,41 @@ class LoginForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput())
     password = forms.CharField(widget=forms.PasswordInput(render_value=False))
     
+class RegisterForm(forms.Form):
+    username = forms.CharField(label='Nombre de usuario',widget=forms.TextInput())
+    email = forms.EmailField(label='Correo',widget=forms.TextInput())
+    password_one = forms.CharField(label='Password',widget=forms.PasswordInput(render_value=False))
+    password_two = forms.CharField(label='Verificar password',widget=forms.PasswordInput(render_value=False))
+    invoice = forms.BooleanField(label='Autorizar facturacion',initial=False,required=False)
+    accounting = forms.BooleanField(label='Autorizar contabilidad',initial=False,required=False)
+    
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        try:
+            u = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return username
+        raise forms.ValidationError('Nombre de usuario ya existe')
+    
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        try:
+            e = User.objects.get(email=email)
+        except User.DoesNotExist:
+            return email
+        raise forms.ValidationError('Email ya registrado')
+    
+    def clean_password_two(self):
+        password_one = self.cleaned_data['password_one']
+        password_two = self.cleaned_data['password_two']
+        if password_one == password_two:
+            pass
+        else:
+            raise forms.ValidationError('Password no coincide')
+        
+    def clean_invoice(self):
+        invoice = self.cleaned_data['invoice']
+        if invoice == None:
+            pass
+        else:
+            return invoice
--- src/home/views.py
+++ src/home/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, render_to_response
 from django.contrib.auth import login, logout, authenticate
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
-from home.forms import LoginForm
+from home.forms import LoginForm, RegisterForm
+from django.contrib.auth.models import User, Group
+
 
 def index_view(request):
     return render_to_response('base.html', context_instance=RequestContext(request))
@@ -34,5 +36,32 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect('/')
+
+def register_view(request):
+    form = RegisterForm()
+    if request.method == 'POST':
+        #con el request.POST obtenemos los datos que vienen del post
+        #y lo almacenamos en la variable form
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            usuario = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password_one = form.cleaned_data['password_one']
+            user = User.objects.create_user(usuario, email, password_one)
+            ''' esto nos servira para designar grupos a los usuarios y poder habilitar o 
+            deshabilitar caracteristicas de la aplicacion 
+            g = Group.objects.get(name='facturacion') 
+            g.user_set.add(user)
+            '''
+            #guardamos el usuario en la base de datos
+            user.save()
+            return render_to_response('home/thanks_register.html',context_instance=RequestContext(request))
+        else:
+            ctx = {'form':form}
+            return render_to_response('home/register.html',ctx,context_instance=RequestContext(request))
+    
+    ctx = {'form':form}
+    return render_to_response('home/register.html',ctx,context_instance=RequestContext(request))
+    
     
     
